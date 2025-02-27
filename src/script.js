9b110c61cc3d50532e6c6f0d819f725fd5b08555
DESKTOP-PGDNCDH\User <sanotos49@gmail.com>
2025-02-27 19:51:44 -0300

    Meu primeiro commit

---
+++ src/script.js
@@ -0,0 +1,17 @@
+function CalcularIdade() {
+    let anoNascimento = document.getElementById("anoNascimento").value;
+    //
+    if (anoNascimento === "") {
+        document.getElementById("resultado").innerHTML = "Por favor, Me fale um ano por gentileza.";
+        return;
+    }
+
+    let anoAtual = new Date().getFullYear();//copia e cola o ano atual
+    let idade = anoAtual - anoNascimento;
+    
+    if (idade > 125) {
+        idade = 125;
+    }//idade limite da operação
+    
+    document.getElementById("resultado").innerHTML = `Sua idade é: ${idade}`;
+}
